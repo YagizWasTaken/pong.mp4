5c0f1810e8456d8d1a5097eca50fe4814eab63d7
Yağız Arslan <107375770+YagizWasTaken@users.noreply.github.com>
2023-08-17 15:31:16 +0300

    Add files via upload

---
+++ pong.py
@@ -0,0 +1,148 @@
+import turtle
+import pygame
+
+
+#music
+file = 'music.wav'
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load(file)
+pygame.mixer.music.play()
+pygame.mixer.music.play(-1)
+
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=1280, height=720)
+wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-450, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(450, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.5
+ball.dy = 0.5
+
+
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+active_paddle = "A"
+paddle_moved = False
+
+# Main game loop
+while True:
+    
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+
+    # Border checking
+
+    # Top and bottom
+    if ball.ycor() > 360:
+        ball.sety(360)
+        ball.dy *= -1
+        #os.system("afplay bounce.wav&")
+    
+    elif ball.ycor() < -360:
+        ball.sety(-360)
+        ball.dy *= -1
+        #os.system("afplay bounce.wav&")
+
+    # Left and right
+ 
+
+    # Paddle and ball collisions
+    if ball.xcor() < -440 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+        ball.dx *= -1 
+        # Paddle B hareket etsin
+        if active_paddle == "A":
+            active_paddle = "B"
+
+
+    elif ball.xcor() > 440 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+        ball.dx *= -1
+        # Paddle A hareket etsin
+        if active_paddle == "B":
+            active_paddle = "A"
+    
+    wn.update()
+    if not paddle_moved:
+        if active_paddle == "A":
+            paddle_a_up()
+        elif active_paddle == "B":
+            paddle_b_up()
+        paddle_moved = True
+    
+    # Eğer bir oyuncu hareket ettiyse, diğer oyuncuya sıra geçsin
+    if paddle_moved:
+        if active_paddle == "A":
+            paddle_b_up()
+        elif active_paddle == "B":
+            paddle_a_up()
+        paddle_moved = False
+
+    paddle_a.sety(ball.ycor())
+    paddle_b.sety(ball.ycor())
+
+
+    paddle_a.sety(ball.ycor())
+    paddle_b.sety(ball.ycor())
+    
+
